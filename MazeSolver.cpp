4fcfe03f039df5b84673e5518ff8892ff273130a
Paula Kurniawan s3782041 <s3782041@student.rmit.edu.au>
2020-03-22 10:47:43 +1100

    Added #defines

--- MazeSolver.cpp
+++ MazeSolver.cpp
@@ -20,25 +20,35 @@ MazeSolver::~MazeSolver()
    //delete breadcrumbs?
 }
 
-#define north     y - 1
-#define east      x + 1
-#define south     y + 1
-#define west      x - 1
+#define NORTH_COOR     y - 1
+#define EAST_COOR      x + 1
+#define SOUTH_COOR     y + 1
+#define WEST_COOR      x - 1
 
+#define NORTH_MAZE     maze[NORTH_COOR][x]
+#define EAST_MAZE      maze[y][EAST_COOR]
+#define SOUTH_MAZE     maze[SOUTH_COOR][x]
+#define WEST_MAZE      maze[y][WEST_COOR]
+
+#define START          'S'
+#define END            'E'
+
+#define Y_COOR_ARRAY          0
+#define X_COOR_ARRAY          1
 
 void MazeSolver::solve(Maze maze) 
 {
-   findCoordinates(maze, 'S', coordinatesPtr);
-   int startY = coordinatesPtr[0];
-   int startX = coordinatesPtr[1];
+   findCoordinates(maze, START, coordinatesPtr);
+   int startY = coordinatesPtr[Y_COOR_ARRAY];
+   int startX = coordinatesPtr[X_COOR_ARRAY];
 
    std::cout << "Start coordinates"<< std::endl;
    std::cout << startX << std::endl;
    std::cout << startY << std::endl;
 
-   findCoordinates(maze, 'E', coordinatesPtr);
-   int endY = coordinatesPtr[0];
-   int endX = coordinatesPtr[1];
+   findCoordinates(maze, END, coordinatesPtr);
+   int endY = coordinatesPtr[Y_COOR_ARRAY];
+   int endX = coordinatesPtr[X_COOR_ARRAY];
 
    std::cout << "End coordinates"<< std::endl;
    std::cout << endX << std::endl;
@@ -49,11 +59,6 @@ void MazeSolver::solve(Maze maze)
    
    bool keepLooping = true; 
 
-   // char north = maze[y - 1][x];
-   // char east = maze[y][x + 1];
-   // char south = maze[y + 1][x];
-   // char west = maze[y][x - 1];
-
    while(keepLooping)
    {
       if(!solution->contains(x,y))
@@ -63,22 +68,42 @@ void MazeSolver::solve(Maze maze)
          solution->addCopy(breadcrumb);
       }
 
-      if(y > 0 && (maze[north][x] == OPEN || maze[north][x] == 'E') && !solution->contains(x,north))
+      // if(y > 0 && (maze[north][x] == OPEN || maze[north][x] == 'E') && !solution->contains(x,north))
+      // {
+      //    std::cout << "North" << std::endl; 
+      //    y--;
+      // }
+      // else if(x < 20 && (maze[y][east] == OPEN || maze[y][east] == 'E') && !solution->contains(east,y))
+      // {
+      //    std::cout << "East" << std::endl;
+      //    x++;
+      // }
+      // else if(y < 20 && (maze[south][x] == OPEN || maze[y][south] == 'E') && !solution->contains(x,south))
+      // {
+      //    std::cout << "South" << std::endl;
+      //    y++;
+      // }
+      // else if(x > 0 && (maze[y][west] == OPEN || maze[y][west] == 'E') && !solution->contains(west,y))
+      // {
+      //    std::cout << "West" << std::endl;
+      //    x--;
+      // }
+      if(y > 0 && (NORTH_MAZE == OPEN || NORTH_MAZE == 'E') && !solution->contains(x,NORTH_COOR))
       {
          std::cout << "North" << std::endl; 
          y--;
       }
-      else if(x < 20 && (maze[y][east] == OPEN || maze[y][east] == 'E') && !solution->contains(east,y))
+      else if(x < 20 && (EAST_MAZE == OPEN || EAST_MAZE == 'E') && !solution->contains(EAST_COOR,y))
       {
          std::cout << "East" << std::endl;
          x++;
       }
-      else if(y < 20 && (maze[south][x] == OPEN || maze[y][south] == 'E') && !solution->contains(x,south))
+      else if(y < 20 && (SOUTH_MAZE == OPEN || SOUTH_MAZE == 'E') && !solution->contains(x,SOUTH_COOR))
       {
          std::cout << "South" << std::endl;
          y++;
       }
-      else if(x > 0 && (maze[y][west] == OPEN || maze[y][west] == 'E') && !solution->contains(west,y))
+      else if(x > 0 && (WEST_MAZE == OPEN || WEST_MAZE == 'E') && !solution->contains(WEST_COOR,y))
       {
          std::cout << "West" << std::endl;
          x--;
@@ -113,8 +138,8 @@ void MazeSolver::findCoordinates(Maze maze, char letter, int* coordinatesPtr)
         {
             if(maze[rows][cols] == letter)
             {
-                coordinatesPtr[0] = rows;
-                coordinatesPtr[1] = cols; 
+                coordinatesPtr[Y_COOR_ARRAY] = rows;
+                coordinatesPtr[X_COOR_ARRAY] = cols; 
             }
         }
     }
